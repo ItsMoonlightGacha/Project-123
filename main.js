0724247b8b81245cd14e075522563dad60b4347f
ItsMoonlightGacha <78887101+ItsMoonlightGacha@users.noreply.github.com>
2021-11-16 22:19:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+nose_X=0;
+nose_Y=0;
+difference=0;
+rightwristX=0;
+leftwristY=0;
+function setup() {
+    canvas=createCanvas(500,500);
+    canvas.position(500,150);
+    background("rgb(145, 194, 223)");
+  }
